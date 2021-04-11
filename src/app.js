5de588c3d94e9e80c7194b8516a6a8eb0fc04006
ArtexCoder <81457945+ArtexCoder@users.noreply.github.com>
2021-04-11 19:27:12 +1000

    Added background photo

--- src/app.js
+++ src/app.js
@@ -66,7 +66,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "7f1abb7499ee1372068a93b1153535be";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
